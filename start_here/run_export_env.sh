b2a35c1313736a45d0e33675eaf1ca23e2e37513
Frank Acquaye <acquayefrank@users.noreply.github.com>
2024-03-08 09:13:57 +0300

    Delete start_here/run_export_env.sh

--- start_here/run_export_env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-pip_packages=$(conda env export | grep -A9999 ".*- pip:" | grep -v "^prefix: ")
-
-conda env export --from-history | grep -v "^prefix: " > environment.yml
-
-echo "$pip_packages" >> environment.yml
